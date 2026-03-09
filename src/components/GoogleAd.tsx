64461bdba56879649619f71bcdb457e03d073406
meka.egwim <egwim.emeka@gmail.com>
2026-03-08 21:33:39 -0700

    GoogleAd Component Update

--- src/components/GoogleAd.tsx
+++ src/components/GoogleAd.tsx
@@ -21,7 +21,7 @@ export const GoogleAd = ({
 
   useEffect(() => {
     const script = document.createElement("script");
-    script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${client}`;
+    script.src = `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${client}`;
     script.async = true;
     script.crossOrigin = "anonymous";
     document.head.appendChild(script);
